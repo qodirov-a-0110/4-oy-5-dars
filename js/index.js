8a8252c563bd60a02b6f7e83efb458dbbbb6ac9c
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-05-27 13:55:52 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,177 @@
+// 1 dan n gacha sonlarni chiqarish
+// 1-misol
+
+// let n = 12;
+// for (let i = 1 ; i<=n; i++){
+//     console.log(i);
+// }
+
+
+
+// 2-misol
+// n dan 1 gacha sonlarni chiqarish
+// let n = 11;
+// for (let i = n ; i>=1; i--){
+//     console.log(i);
+// }
+
+// 3-misol
+// 1 dan n gacha sonlar yi g'indisi
+// let n = 11;
+// let sum = 0
+// for (let i = 1 ; i<=n; i++){
+//     sum = sum +i;  
+// }
+// console.log(sum);
+
+// 4-misol
+// let n = 11;
+// let counter = 0;
+// let sum = 0
+// for (let i = 2 ; i<=n; i= i+2){
+//     console.log(i);
+//     sum = sum +i; 
+// }
+// console.log(sum);
+
+// 5-misol
+// let n = 11;
+// let counter = 0;
+// let sum = 0
+// for (let i = 1 ; i<=n; i= i+2){
+//     console.log(i);
+//     sum = sum +i; 
+// }
+// console.log(sum);
+
+// Sonning bo' luvchilarini chiqarish
+// 6-misol
+// let n = 25;
+// let counter=0;
+// for(let i=1; i<=n; i++){
+//     if(n%i==0 ){
+//         console.log(i);
+//     }
+// 7-misol
+// 7-masala
+// let A = +prompt('Boluvcilari chiqarilishi kerak bolgan son');
+// let counter = 0;
+// let summ = 0;
+// for (let i = 1; i <=A; i++) {
+//     if (A % i == 0) {
+//     summ = summ + i;
+//     counter = counter + 1;
+//     }
+// }
+
+
+
+// 8-misol
+// let n = 25;
+// let counter=0;
+// for(let i=1; i<=n; i++){
+//     if(n%i==0 ){
+//         counter=counter+1;
+//     }
+// }
+// if(counter>2){
+//     console.log(`murakkab son`);
+// }else {
+//     console.log(`tub son`);
+// }
+
+// 1.1-misol;;
+// let k = 24;
+// let n = 26;
+// for (let i = 1; i <=n; i++) {
+//     console.log(k);
+// }
+
+//1.2-misol;;
+// let a = 78;
+// let b = 41;
+// let counter = 0;
+// for (let i = a; i <= b; i++) {
+//     counter =  counter + 1;
+// }
+// console.log(i);
+// console.log(counter);
+
+// 1.3-misol;;
+// let a = 11;
+// let b = 23;
+// let counter = 0;
+// for (let i = b - 1; i < a + 1; i--) {
+//     counter =  counter + 1;
+// }
+// console.log(i);
+// console.log(counter);
+
+// 1.4-misol;
+// let narx = +prompt('1 kg konfet narxi');
+// for (let i = 1; i <= 10; i++) {
+//     console.log(narx * i);
+// }
+
+// 1.5-misol;
+// let narx = +prompt('1 kg konfet narxi');
+// for (let i = 0.1; i <= 1; i += 0.1) {
+//     console.log(narx * i);
+// }
+
+//1.6-misol;
+// let narx = +prompt('1 kg konfet narxi');
+// for (let i = 1.2; i <= 2; i += 0.2) {
+//     console.log(narx * i);
+// }
+
+//1.7-misol;
+// let a = 55;
+// let b = 36;
+// let summ = 0;
+// for (let i = a; i <= b; i++) {
+//     summ = summ + i;
+// }
+// console.log(summ);
+
+// 1.8-misol;
+// let a = 45;
+// let b = 49;
+// let p = 1;
+// for (let i = a; i <= b; i++) {
+//     p = p * i;
+// }
+// console.log(p);
+
+//1.9-misol;
+// let a = 89;
+// let b = 21;
+// let summ = 0;
+// for (let i = a; i <= b; i++) {
+//     summ = summ + i**2;
+// }
+// console.log(summ);
+
+// 1.10-misol;
+// let n = 41;
+// let S = 1;
+// for (let i = 1; i <= n; i++) {
+//     S = S + 1/i;
+// }
+// console.log(S);
+
+// 1.11-misol;
+// let n = 35;
+// let S = 0;
+// for (let i = n; i <= 2*n; i++) {
+//     S = S + i**2;
+// }
+// console.log(S);
+
+// 1.12-misol;
+// let n = 12;
+// let P = 1;
+// for (let i = 1.1; i <= n; i += 0.1) {
+//     P = P * i;
+// }
+// console.log(P);
